7564c2dc64e8da679d41ecaf9b97013fb536a62b
beatrizrecari@gmail.com <beatrizrecari@Beatrizs-MacBook-Pro.local>
2023-01-17 12:57:27 -0500

    updated queries

--- mysql-js/index.js
+++ mysql-js/index.js
@@ -19,7 +19,11 @@ db.query('UPDATE users SET first_name = "Cass" WHERE first_name = "Jiho"', (err)
 db.query("SELECT * FROM users", (err, results) => {
     if(err) console.log("ERROR -> ", err);
     console.table(results);
+
 });
 
+db.end();
+
 console.log("The end.")
+
 
